af3fe4c9a0cc8b548ca92a08fa794ffc5e3debf2
Piyush <cpiyush170@gmail.com>
2022-12-29 15:55:53 +0530

    feed-a-mole

---
+++ feed-a-mole/game.js
@@ -0,0 +1,174 @@
+let score = 0;
+
+function getSadInterval() {
+    return Date.now() + 1000;
+}
+function getGoneInterval() {
+    return Date.now() + Math.floor(Math.random() * 18000) + 2000;
+}
+
+
+function getHungryInterval() {
+    return Date.now() + Math.floor(Math.random() * 3000) + 2000;
+}
+
+function getKingStatus() {
+    return Math.random() > .9;
+}
+// array of mole objects
+const moles = [
+    {
+        status: "sad",
+        next: getSadInterval(),
+        king: false,
+        node: document.getElementById("hole-0")
+    },
+    {
+        status: "sad",
+        next: getSadInterval(),
+        king: false,
+        node: document.getElementById("hole-1")
+    },
+    {
+        status: "sad",
+        next: getSadInterval(),
+        king: false,
+        node: document.getElementById("hole-2")
+    },
+    {
+        status: "sad",
+        next: getSadInterval(),
+        king: false,
+        node: document.getElementById("hole-3")
+    },
+    {
+        status: "sad",
+        next: getSadInterval(),
+        king: false,
+        node: document.getElementById("hole-4")
+    },
+    {
+        status: "sad",
+        next: getSadInterval(),
+        king: false,
+        node: document.getElementById("hole-5")
+    },
+    {
+        status: "sad",
+        next: getSadInterval(),
+        king: false,
+        node: document.getElementById("hole-6")
+    },
+    {
+        status: "sad",
+        next: getSadInterval(),
+        king: false,
+        node: document.getElementById("hole-7")
+    },
+    {
+        status: "sad",
+        next: getSadInterval(),
+        king: false,
+        node: document.getElementById("hole-8")
+    },
+    {
+        status: "sad",
+        next: getSadInterval(),
+        king: false,
+        node: document.getElementById("hole-9")
+    }
+];
+
+function getNextStatus(mole) {
+    switch (mole.status) {
+        case "sad":
+        case "fed":
+            mole.next = getSadInterval();
+            mole.status = "leaving";
+            if (mole.king) {
+                mole.node.children[0].src = "./images/king-mole-leaving.png";
+            }
+            else {
+                mole.node.children[0].src = "./images/mole-leaving.png";
+            }
+            break;
+        case "leaving":
+            mole.next = getGoneInterval();
+            mole.status = "gone";
+            mole.node.children[0].classList.add("gone");
+            break;
+        case "gone":
+            mole.status = "hungry";
+            mole.next = getHungryInterval();
+            mole.king = getKingStatus();
+            mole.node.children[0].classList.remove("gone");
+            mole.node.children[0].classList.add("hungry");
+            if (mole.king) {
+                mole.node.children[0].src = "./images/king-mole-hungry.png";
+            }
+            else {
+                mole.node.children[0].src = "./images/mole-hungry.png";
+            }
+            break;
+        case "hungry":
+            mole.status = "sad";
+            mole.next = getSadInterval();
+            mole.node.children[0].classList.remove("hungry");
+            if (mole.king) {
+                mole.node.children[0].src = "./images/king-mole-sad.png";
+            }
+            else {
+                mole.node.children[0].src = "./images/mole-sad.png";
+            }
+            break;
+    }
+}
+
+function feed(e) {
+
+    if (e.target.tagName !== "IMG" ||
+        !e.target.classList.contains("hungry")) return;
+
+    const mole = moles[parseInt(e.target.dataset.index)]
+    mole.status = "fed";
+    mole.next = getSadInterval();
+    if (mole.king) {
+        mole.node.children[0].src = "./images/king-mole-fed.png";
+        score += 2;
+    }
+    else {
+        mole.node.children[0].src = "./images/mole-fed.png";
+        score++;
+    }
+    mole.node.children[0].classList.remove("hungry");
+
+    if (score >= 10) {
+        win();
+    }
+
+    document.querySelector(".worm-container").style.width = `${10 * score}%`
+}
+function win() {
+    document.querySelector('.bg').classList.add("hide");
+    document.querySelector('.win').classList.remove("hide");
+}
+
+let runAgainAt = Date.now() + 100;
+
+function nextFrame() {
+    const now = Date.now();
+
+    if (runAgainAt <= now) {
+        for (let i = 0; i < moles.length; i++) {
+            if (moles[i].next <= now) {
+                getNextStatus(moles[i]);
+            }
+        }
+        runAgainAt = now + 100;
+    }
+    requestAnimationFrame(nextFrame);
+}
+
+document.querySelector(".bg").addEventListener("click", feed);
+
+nextFrame();
